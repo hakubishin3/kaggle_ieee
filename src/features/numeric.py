81608c1045fafaec940160ad0058fbe9aef65ec0
hakubishin3 <goudasp2001@yahoo.co.jp>
2019-08-29 11:35:14 +0000

    model_6

--- src/features/numeric.py
+++ src/features/numeric.py
@@ -51,6 +51,7 @@ def get_numeric_cols():
     main_numeric_cols = ["TransactionAmt", "dist1", "dist2"]
     id_numeric_cols = get_id_numeric_cols()
     c_cols = get_C_cols()
+    d_cols = get_D_cols()
 
     """
     numeric_cols = [
@@ -64,8 +65,8 @@ def get_numeric_cols():
     ]
     """
 
-    numeric_cols = main_numeric_cols + id_numeric_cols + c_cols
-    remove_cols = []
+    numeric_cols = main_numeric_cols + id_numeric_cols + c_cols + d_cols
+    remove_cols = ['id_02', 'C9', 'C11', 'C12', 'C13']
     numeric_cols = [col for col in numeric_cols if col not in remove_cols]
 
     return numeric_cols
